7f7e4712252aa9cdf06a3e0ff1ba4680ab058ef1
Eugene <eliseevmail@ya.ru>
2022-06-02 00:35:59 +0300

    Добавьте файлы проекта.

---
+++ Eliseev.EFTranslations.App/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Eliseev.EFTranslations.App;
+
+Console.WriteLine("Hello, World!");
+
+
+DataContext dataContext = new DataContext();
+
+var a = dataContext.SomeEntities.ToList();
+
+var b = 1;
